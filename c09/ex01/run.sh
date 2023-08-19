6c0e5af46f7638c1fc853af4ccc1076e7fd93abf
Gabriel Bazart <gbazart@bess-f2r4s16.clusters.42paris.fr>
2023-08-19 20:55:28 +0200

    run.sh gitignore

---
+++ c09/ex01/run.sh
@@ -0,0 +1,18 @@
+if 	[ ! -f "./srcs/ft_putchar.o" ];then
+	echo "ft_putchar.o n'est pas au bon endroit !";
+fi
+if 	[ ! -f "./srcs/ft_putstr.o" ];then
+	echo "ft_putstr.o n'est pas au bon endroit !";
+fi
+if 	[ ! -f "./srcs/ft_strcmp.o" ];then
+	echo "ft_strcmp.o n'est pas au bon endroit !";
+fi
+if 	[ ! -f "./srcs/ft_strlen.o" ];then
+	echo "ft_strlen.o n'est pas au bon endroit !";
+fi
+if 	[ ! -f "./srcs/ft_swap.o" ];then
+	echo "ft_swap.o n'est pas au bon endroit !";
+fi
+if 	[ ! -f "./libft.a" ];then
+	echo "libft.a n'est pas au bon endroit !";
+fi
